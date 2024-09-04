411fd372641fe92a70a1e746c54b260a4dd4ac2d
minahilmehdi <minahilmehdi45@gmail.com>
2024-09-04 11:51:01 +0000

    Typo correction

--- streamlitapp.py
+++ streamlitapp.py
@@ -31,7 +31,7 @@ st.write(f"Model: {accuracy}")
 st.header("Real-Time Prediction")
 input_data = []
 for col in X_test.columns:
-    input_value = st.numner_input(f'Input for feature{col}', value='')
+    input_value = st.number_input(f'Input for feature{col}', value='')
     input_data.append(input_value)
 
 #Convert input data to dataframe
@@ -41,4 +41,3 @@ input_df = pd.DataFrame([input_data], columns=X_test.columns)
 if st.button("Predict"):
     prediction = model.predict(input_df)
     st.write(f'Prediction: {prediction[0]}')
-    
